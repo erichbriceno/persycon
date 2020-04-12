28da6e2553e21ec21d0e9f1f0ddc060afe4d257c
Erich Briceno <erichbriceno@gmail.com>
2020-04-11 23:44:01 -0400

    Change of title in projects

--- resources/views/project/projects.blade.php
+++ resources/views/project/projects.blade.php
@@ -12,7 +12,7 @@
                 <th scope="col">#</th>
                 <th scope="col">@lang('Name')</th>
                 <th scope="col">@lang('Description')</th>
-                <th colspan="4" scope="col" class="text-center">@lang('Hired')</th>
+                <th colspan="4" scope="col" class="text-center">@lang('Staff')</th>
                 <th scope="col" class="text-center">@lang('Dates')</th>
                 <th scope="col" class="text-center">@lang('Actions')</th>
             </tr>
